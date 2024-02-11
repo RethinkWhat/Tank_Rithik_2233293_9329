d2db2ae72b9a51e8c2f9cf7fb31671a501b815cf
Rithik Tank <2233293@slu.edu.ph>
2024-02-11 09:07:43 +0800

    Added comments

--- pexer3/PreExercise3Client.java
+++ pexer3/PreExercise3Client.java
@@ -1,33 +1,61 @@
 package pexer3;
 
+// W3C Imports
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.UserDataHandler;
-import org.w3c.dom.ls.LSOutput;
+import org.xml.sax.SAXException;
 
+// IO Imports
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+
+// XML Parser Import
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.*;
-import java.net.ServerSocket;
+import javax.xml.parsers.ParserConfigurationException;
+
+// Net imports
 import java.net.Socket;
-import java.sql.SQLOutput;
+
+// Util Imports
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 
+
+/**
+ * PreExercise3Client class
+ */
 public class PreExercise3Client implements Runnable{
 
+    // Class variable to hold the client socket
     Socket client;
 
+    // Variable to hold the lines read in the xml
     int lineRead = 0;
 
 
+    DocumentBuilder builder;
+    Document document;
+    NodeList nodeList;
 
+
+    /**
+     * Constructor
+     * @param client
+     * @throws IOException
+     */
     public PreExercise3Client(Socket client) throws IOException{
         this.client = client;
     }
 
+    /**
+     * Method to read line from server
+     * @return lineRead
+     */
     public String readFromServer() {
         String nextLine = null;
         try {
@@ -40,65 +68,119 @@ public class PreExercise3Client implements Runnable{
     }
 
 
+    /**
+     * Method to write line to server
+     * @param line : String
+     * @throws IOException
+     */
     public void writeToServer(String line) throws IOException {
         PrintWriter writer = new PrintWriter(new OutputStreamWriter(client.getOutputStream()),true);
         writer.println(line);
     }
 
+    /**
+     * Method to get the client socket
+     * @return
+     */
     public Socket getClient() {
         return client;
     }
 
+    /**
+     * Method to set the client socket
+     * @param client : Socket
+     */
     public void setClient(Socket client) {
         this.client = client;
     }
 
-    public void readLineFromFile() throws IOException {
-        List<String> expression = null;
-        try {
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document document = builder.parse("res/exer3.xml");
-            document.getDocumentElement().normalize();
-
-            NodeList nodeList = document.getElementsByTagName("expression");
-
-            Node first = nodeList.item(lineRead);
-            if (first == null) {
-                writeToServer("done");
-                return;
+    /**
+     * Method to read line from file
+     * @return boolean lineRead
+     * @throws IOException
+     *
+     * Algorithm:
+     *      1. Get the node at variable lineRead
+     *      2. If the node is null tell the server all the lines have been read
+     *          2.1. Return true so the loop that calls this method knows the reading has been complete
+     *      3. Get the children node of the chosen node
+     *          3.1. Iterate through children node and add the text content of each node to a list
+     *      4. Get all the elements in the list and concatenate them to a single string and write to server
+     */
+    public boolean readLineFromFile() throws IOException {
+        List<String> expression;
+        Node first = nodeList.item(lineRead);
+        if (first == null) {
+            writeToServer("bye");
+            return true;
+        }
+        NodeList children = first.getChildNodes();
+        expression = new ArrayList<>();
+        for (int y = 0; y < children.getLength(); y++) {
+            if (children.item(y).getNodeType() == Node.ELEMENT_NODE) {
+                expression.add(children.item(y).getTextContent());
             }
-            NodeList children = first.getChildNodes();
-            expression = new ArrayList<>();
-            for (int y = 0; y < children.getLength(); y++) {
-                if (children.item(y).getNodeType() == Node.ELEMENT_NODE) {
-                    expression.add(children.item(y).getTextContent());
-                }
+        }
+        lineRead++;
+        String toReturn = "";
+        for (String term : expression) {
+            toReturn += term + ",";
+        }
+        writeToServer(toReturn);
+        return false;
+    }
+
+    /**
+     * Method to handle constructing the document
+     * @throws IOException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     *
+     * Algorithm:
+     *      1. Create a documentBuilder object
+     *      2. Create a Document object passing in the exer3.xml file
+     *      3. Normalize the elements of the document
+     *      4. Create a nodeList, getting the elements by the tag, "expression."
+     */
+    public void constructDocument() throws IOException, ParserConfigurationException, SAXException {
+        builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        document = builder.parse("res/exer3.xml");
+        document.getDocumentElement().normalize();
+
+        nodeList = document.getElementsByTagName("expression");
+    }
+
+
+    /**
+     * Run method to handle receiving and sending from client
+     *
+     * Algorithm:
+     *     1. Call the constructDocument method
+     *     2. Create a while loop
+     *          2.1. Call readLineFromFile() and if return value is true break out of loop
+     *          2.2. Read line from server and print line
+     */
+    public void run() {
+        String serverResponse = "";
+        boolean finished;
+        try {
+            constructDocument();
+            while (true) {
+                finished = readLineFromFile();
+                if (finished)
+                    break;
+                serverResponse = readFromServer();
+                System.out.println(serverResponse);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        lineRead++;
-        String toReturn = "";
-        for (String term: expression) {
-            toReturn += term +",";
-        }
-        writeToServer(toReturn);
     }
 
-
-    public void run() {
-        String serverResponse = "";
-        try {
-            while (!serverResponse.equalsIgnoreCase("bye")) {
-                readLineFromFile();
-                serverResponse = readFromServer();
-                System.out.println(serverResponse);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
+    /**
+     * Main method to run a client object
+     * @param args : String[]
+     */
     public static void main(String[] args) {
         try {
             Socket clientSocket = new Socket("localhost", 2020);
--- pexer3/PreExercise3Server.java
+++ pexer3/PreExercise3Server.java
@@ -5,17 +5,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * PreExercise3Server class
+ */
 public class PreExercise3Server {
 
+    // ServerSocket class variable
     private ServerSocket server;
 
-
+    /**
+     * Class Construct
+     * @param port : int
+     * @throws IOException
+     */
     public PreExercise3Server(int port) throws IOException {
         server = new ServerSocket(port);
     }
 
+    /**
+     * Method to read String from a client
+     * @param client : Socket
+     * @return lineRead
+     *
+     * Algorithm:
+     *      1. Create a reader object and pass in client input stream
+     *      2. Call the readLine method of the reader object and return the value
+     */
     public static String readFromClient(Socket client) {
         String nextLine = null;
         try {
@@ -27,6 +43,16 @@ public class PreExercise3Server {
         return nextLine;
     }
 
+    /**
+     * Method to write String to client
+     * @param client : Socket
+     * @param line : String
+     * @return void
+     *
+     * Algorithm:
+     *      1. Create a writer object and pass in client output stream
+     *      2. Call the println method of the writer object
+     */
     public static void writeToClient(Socket client, String line) {
         try {
             PrintWriter writer = new PrintWriter(new OutputStreamWriter(client.getOutputStream()),true);
@@ -36,14 +62,35 @@ public class PreExercise3Server {
         }
     }
 
+    /**
+     * Getter of the server object
+     * @return server
+     */
     public ServerSocket getServer() {
         return server;
     }
 
+    /**
+     * Setter of the server object
+     * @param server : ServerSocket
+     */
     public void setServer(ServerSocket server) {
         this.server = server;
     }
 
+    /**
+     * Method to solve an expression given it is passed us a comma seperated String
+     * @param toSolve : String
+     * @return solvedExpression
+     *
+     * Algorithm:
+     *      1. Split the passed in expression using the regex ','
+     *      2. If the expression length is not exactly 3 return null for error
+     *      3. Otherwise, create a switch statement that looks at the operator
+     *          3.1. Depending on the string representation of the operator perform the appropriate solving
+     *          3.2. If operator passed in is not a valid operator, return a message indicating as such
+     *          3.3. If solving is successful, return the formatted expression and the result
+     */
     public static String solveExpression(String toSolve) {
         double solved;
         String[] expression = toSolve.split(",");
@@ -71,30 +118,40 @@ public class PreExercise3Server {
         return expression[0] + " " + expression[1] + " " + expression[2] + " = " + solved;
     }
 
+    /**
+     * Main method to handle running the server
+     *
+     * Algorithm:
+     *      1. Create a PreExercise3Server obj
+     *      2. Accept a connection for the serverSocket
+     *      3. Create a thread which will handle running the PreExercise3Client obj
+     *      3. Create a while loop
+     *          3.1. Read a line from client using the readFromClient method
+     *          3.2. If the value of the line read is "bye," exit loop
+     *          3.3. Otherwise, call the solveExpression method on the passed in line
+     *          3.4. Send the solved expression to Client using the writeToClient method
+     * @param args
+     */
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
         PreExercise3Server server;
         Socket client = null;
         String expressionToSolve = "";
 
-            try {
-                server = new PreExercise3Server(2020);
+        try {
+            server = new PreExercise3Server(2020);
 
-                while (true) {
-                    client = server.getServer().accept();
-                    new Thread(new PreExercise3Client(client));
+            client = server.getServer().accept();
+            new Thread(new PreExercise3Client(client));
 
-                    while (true) {
-                        expressionToSolve = readFromClient(client);
-                        if (expressionToSolve.equals("done"))
-                            break;
-                        writeToClient(client, solveExpression(expressionToSolve));
-                    }
-                    writeToClient(client, "bye");
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
+            while (true) {
+                expressionToSolve = readFromClient(client);
+                if (expressionToSolve.equals("bye"))
+                    break;
+                writeToClient(client, solveExpression(expressionToSolve));
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
